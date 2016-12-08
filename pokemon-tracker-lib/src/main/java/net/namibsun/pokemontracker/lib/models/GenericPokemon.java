ce319d99388b89197626984a061a0ad716c86399
Hermann Krumrey <hermann@krumreyh.com>
2016-12-08 22:36:56 +0100

    Integrated Jsoup and the name.

--- pokemon-tracker-lib/src/main/java/net/namibsun/pokemontracker/lib/models/GenericPokemon.java
+++ pokemon-tracker-lib/src/main/java/net/namibsun/pokemontracker/lib/models/GenericPokemon.java
@@ -1,5 +1,6 @@
 package net.namibsun.pokemontracker.lib.models;
 
+import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import net.namibsun.pokemontracker.lib.models.pokemonparts.Name;
 
@@ -10,20 +11,38 @@ import java.io.IOException;
  */
 public class GenericPokemon {
 
+    /**
+     * The national Pokedex number of this Pokemon
+     */
     private int pokedexNumber;
+
+    /**
+     * The name of this Pokemon
+     */
     private Name name;
 
+    /**
+     * Creates a new Pokemon
+     * @param pokedexNumber: The national Pokedex Number to identify the Pokemon
+     */
     public GenericPokemon(int pokedexNumber) {
         this.pokedexNumber = pokedexNumber;
 
         Document serebiiPage;
         try {
             serebiiPage = Jsoup.connect("http://www.serebii.net/pokedex-sm/" + pokedexNumber + "722.shtml").get();
+            this.getOnlineInfo(serebiiPage);
         } catch (IOException e) {
             serebiiPage = null;
         }
+    }
 
-        this.name = Name.fromSerebiiPage()
+    /**
+     * Fetches the online information for the Pokemon
+     * @param serebiiPage: The serebii page document
+     */
+    private void getOnlineInfo(Document serebiiPage) {
+        this.name = Name.fromSerebiiPage(serebiiPage);
     }
 
 }
