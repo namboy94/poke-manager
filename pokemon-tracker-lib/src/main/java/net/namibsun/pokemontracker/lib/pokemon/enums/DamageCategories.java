599db11a5ff710334626e0de8ad5ce719a926da4
Hermann Krumrey <hermann@krumreyh.com>
2017-11-20 14:29:04 +0100

    Remove legacy source files

--- pokemon-tracker-lib/src/main/java/net/namibsun/pokemontracker/lib/pokemon/enums/DamageCategories.java
+++
@@ -1,8 +0,0 @@
-package net.namibsun.pokemontracker.lib.pokemon.enums;
-
-/**
- * Enum that defines the types a Pokemon Move can be (Special, Physical etc.)
- */
-public enum DamageCategories {
-    PHYSICAL, SPECIAL, STATUS
-}
